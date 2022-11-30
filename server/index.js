0b18f386f4908b8d8fb28f8bac9823c840d6bc2b
Steve Moritani <steve.hayato.m@gmail.com>
2022-11-29 17:52:45 -0800

    Added more types and connecting to MongoDB

--- server/index.js
+++ server/index.js
@@ -2,8 +2,12 @@ const express = require("express");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 8000;
 const app = express();
+
+//Connect to database
+connectDB();
 
 app.use(
   "/graphql",
