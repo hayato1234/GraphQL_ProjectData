4eb703000f86b84c25f45f262be165b408ab9352
Steve Moritani <steve.hayato.m@gmail.com>
2022-11-30 16:07:07 -0800

    Add project func added

---
+++ client/src/components/AddProjectModal.jsx
@@ -0,0 +1,159 @@
+import { FaList } from "react-icons/fa";
+import { useState } from "react";
+import { GET_PROJECTS } from "../queries/projectQueries";
+import { useMutation, useQuery } from "@apollo/client";
+import { GET_CLIENTS } from "../queries/clientQueries";
+import { ADD_PROJECT } from "../mutations/projectMutation";
+
+export default function AddProjectModal() {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [clientId, setClientId] = useState("");
+  const [status, setStatus] = useState("new");
+
+  const [addProject] = useMutation(ADD_PROJECT, {
+    variables: { name, description, clientId, status },
+    update(cache, { data: { addProject } }) {
+      const { projects } = cache.readQuery({ query: GET_PROJECTS });
+      cache.writeQuery({
+        query: GET_PROJECTS,
+        data: { projects: [...projects, addProject] },
+      });
+    },
+  });
+
+  //   const [addClient] = useMutation(ADD_CLIENT, {
+  //     variables: { name, email, phone },
+  //     update(cache, { data: { addClient } }) {
+  //       const { clients } = cache.readQuery({ query: GET_CLIENTS });
+  //       cache.writeQuery({
+  //         query: GET_CLIENTS,
+  //         data: { clients: [...clients, addClient] },
+  //       });
+  //     },
+  //   });
+
+  //Get Clients for select
+  const { loading, error, data } = useQuery(GET_CLIENTS);
+
+  if (loading) return null;
+  if (error) return "Something went wrong";
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    // console.log(name, email, phone);
+    if (name === "" || description === "" || status === "") {
+      return alert("Please fill in all fields");
+    }
+
+    addProject(name, description, clientId, status);
+    setName("");
+    setDescription("");
+    setStatus("new");
+    setClientId("");
+  };
+  return (
+    <>
+      {!loading && !error && (
+        <>
+          {/* <!-- Button trigger modal --> */}
+          <button
+            type="button"
+            className="btn btn-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#addProjectModal"
+          >
+            <div className="d-flex align-items-center">
+              <FaList className="icon" />
+              <div>New Project</div>
+            </div>
+          </button>
+
+          {/* <!-- Modal --> */}
+          <div
+            className="modal fade"
+            id="addProjectModal"
+            aria-labelledby="addProjectModalLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title" id="exampleModalLabel">
+                    New Project
+                  </h5>
+                  <button
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  ></button>
+                </div>
+                <div className="modal-body">
+                  <form onSubmit={onSubmit}>
+                    <div className="mb-3">
+                      <label className="form-label">name</label>
+                      <input
+                        id="name"
+                        type="text"
+                        className="form-control"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <label className="form-label">Description</label>
+                      <textarea
+                        id="description"
+                        className="form-control"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <label className="form-label">Status</label>
+                      <select
+                        id="status"
+                        className="form-select"
+                        value={status}
+                        onChange={(e) => setStatus(e.target.value)}
+                      >
+                        <option value="new">Not Started</option>
+                        <option value="progress">In Progress</option>
+                        <option value="completed">Completed</option>
+                      </select>
+                    </div>
+
+                    <div className="mb-3">
+                      <label className="form-label">Client</label>
+                      <select
+                        id="clientId"
+                        value={clientId}
+                        onChange={(e) => setClientId(e.target.value)}
+                        className="form-select"
+                      >
+                        <option value="">Select Client</option>
+                        {data.clients.map((client) => (
+                          <option key={client.id} value={client.id}>
+                            {client.name}
+                          </option>
+                        ))}
+                      </select>
+                    </div>
+                    <button
+                      type="submit"
+                      data-bs-dismiss="modal"
+                      className="btn btn-primary"
+                    >
+                      Submit
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+}
